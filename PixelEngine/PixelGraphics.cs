f3fffdf59f93ad41c2eed416167de20a9633c20f
viktor schewenius <viktorschewenius@gmail.com>
2022-11-01 09:39:29 +0100

    Upgraded PixelGrahics To Be A Lot Faster

--- PixelEngine/PixelGraphics.cs
+++ PixelEngine/PixelGraphics.cs
@@ -1,13 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace PixelEngine
 {
+    public class DirectBitmap : IDisposable
+    {
+        public Bitmap Bitmap { get; private set; }
+        public Int32[] Bits { get; private set; }
+        public bool Disposed { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        protected GCHandle BitsHandle { get; private set; }
+
+        public DirectBitmap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Bits = new Int32[width * height];
+            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
+            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
+        }
+
+        public void SetPixel(int x, int y, Color colour)
+        {
+            int index = x + (y * Width);
+            int col = colour.ToArgb();
+
+            Bits[index] = col;
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            int index = x + (y * Width);
+            int col = Bits[index];
+            Color result = Color.FromArgb(col);
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (Disposed) return;
+            Disposed = true;
+            Bitmap.Dispose();
+            BitsHandle.Free();
+        }
+    }
+
+
     internal class PixelGraphics
     {
         readonly Color[] PixelArray = new Color[640 * 640];
@@ -91,7 +139,7 @@ namespace PixelEngine
 
         public void Draw(Graphics g)
         {
-            Bitmap bitmap = new Bitmap(640, 640, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            DirectBitmap bitmap = new DirectBitmap(640, 640);
             for (int y = 0; y < 640; y++)
             {
                 for (int x = 0; x < 640; x++)
@@ -99,7 +147,7 @@ namespace PixelEngine
                     bitmap.SetPixel(x, y, PixelArray[y * 640 + x]);
                 }
             }
-            g.DrawImage(bitmap, 0, 0);
+            g.DrawImage(bitmap.Bitmap, 0, 0);
         }
     }
 }
